e2aa734b1051ecd12477b57dc56d4b1f9954d5bc
Antonio Glaybson <gleybsonmelo998@gmail.com>
2023-04-24 09:04:38 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const feedRoutes = require("./routes/feed");
+
+app.use(bodyParser.json());
+app.use("/feed", feedRoutes);
+
+app.listen(8080, () => {
+    console.log("server on.");
+})
---
+++ controllers/feed.js
@@ -0,0 +1,5 @@
+exports.getFeed = (req, res, next) => {
+    res.json({
+        message: "Hello World!"
+    });
+}
